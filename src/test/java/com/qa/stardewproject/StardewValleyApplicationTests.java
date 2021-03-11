db823760e34a62012e45de7a8b351024ea259207
Emily Nixon <enixon@qa.com>
2021-03-11 11:47:26 +0000

    Adding Test and villager controllers

---
+++ src/test/java/com/qa/stardewproject/StardewValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.stardewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StardewValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
